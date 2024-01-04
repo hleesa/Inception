4456964030b21800bc78ff907c4b0f2fecd84444
Sanghoon Lee <salee2@c6r9s3.42seoul.kr>
2024-01-04 20:56:55 +0900

    add

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -20,9 +20,13 @@ else
 	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$WORDPRESS_DB_HOST/g" wp-config-sample.php
 	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config-sample.php
+
 	cp wp-config-sample.php wp-config.php
 ###################################
 
+    wp core install --url="$DOMAIN_NAME" --title=INCEPTION --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL"  --allow-root  
+    wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=author --allow-root 
+	
 fi
 
 exec "$@"
