a4c9fd27e4abd023be504001d7676e15d87d9752
lsh <salee2@student.42seoul.kr>
2024-01-20 17:12:51 +0900

    fffin

--- Makefile
+++ Makefile
@@ -8,13 +8,10 @@ re:
 	@docker-compose -f srcs/docker-compose.yml up -d --build
 
 clean:
-	@docker rm -f $$(docker ps -qa)
+	docker-compose -f ./srcs/docker-compose.yml down
 	@docker volume rm -f $$(docker volume ls)
-
-fclean:
-	make clean
 	docker system prune --volumes --all --force
 	docker network prune --force
 	docker volume prune --force
 
-.PHONY: all re down clean fclean
+.PHONY: all re down clean
--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,31 +1,16 @@
 #!/bin/sh
 
-if [ -d "/run/mysqld" ]; then
-	chown -R mysql:mysql /run/mysqld
-else
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+service mariadb start 
 
-if [ -d /var/lib/mysql/mysql ]; then
-	chown -R mysql:mysql /var/lib/mysql
-else
-	chown -R mysql:mysql /var/lib/mysql
-	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-	mariadbd --user=mysql --bootstrap --skip-networking=0 << eof
-USE mysql;
-FLUSH PRIVILEGES;
-GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');
-DROP DATABASE IF EXISTS test;
-FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-FLUSH PRIVILEGES;
-eof
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql
 
-fi
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
 
-exec mariadbd --user=mysql --skip-networking=0
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+
+echo "FLUSH PRIVILEGES;" | mysql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
