cc715b21a9814ec52bb99b7f35034b80a9f3df6a
lsh <salee2@student.42seoul.kr>
2024-01-20 14:33:44 +0900

    fin

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,49 +1,31 @@
 #!/bin/sh
 
-service mariadb start 
+if [ -d "/run/mysqld" ]; then
+	chown -R mysql:mysql /run/mysqld
+else
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql
+if [ -d /var/lib/mysql/mysql ]; then
+	chown -R mysql:mysql /var/lib/mysql
+else
+	chown -R mysql:mysql /var/lib/mysql
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+	mariadbd --user=mysql --bootstrap --skip-networking=0 << eof
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');
+DROP DATABASE IF EXISTS test;
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+FLUSH PRIVILEGES;
+eof
 
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+fi
 
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
-
-echo "FLUSH PRIVILEGES;" | mysql
-
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
-
-
-# #!/bin/sh
-
-# if [ -d "/run/mysqld" ]; then
-# 	chown -R mysql:mysql /run/mysqld
-# else
-# 	mkdir -p /run/mysqld
-# 	chown -R mysql:mysql /run/mysqld
-# fi
-
-# if [ -d /var/lib/mysql/mysql ]; then
-# 	chown -R mysql:mysql /var/lib/mysql
-# else
-# 	chown -R mysql:mysql /var/lib/mysql
-# 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-# 	mariadbd --user=mysql --bootstrap --skip-networking=0 << eof
-# USE mysql;
-# FLUSH PRIVILEGES;
-# GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-# GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
-# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');
-# DROP DATABASE IF EXISTS test;
-# FLUSH PRIVILEGES;
-# CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-# FLUSH PRIVILEGES;
-# eof
-
-# fi
-
-# exec mariadbd --user=mysql --skip-networking=0
+exec mariadbd --user=mysql --skip-networking=0
