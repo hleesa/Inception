3809b41de65a224fb9342c2a52711c9f1824f121
lsh <salee2@student.42seoul.kr>
2024-01-20 12:55:49 +0900

    edit

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -14,3 +14,36 @@ echo "FLUSH PRIVILEGES;" | mysql
 kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld
+
+
+# #!/bin/sh
+
+# if [ -d "/run/mysqld" ]; then
+# 	chown -R mysql:mysql /run/mysqld
+# else
+# 	mkdir -p /run/mysqld
+# 	chown -R mysql:mysql /run/mysqld
+# fi
+
+# if [ -d /var/lib/mysql/mysql ]; then
+# 	chown -R mysql:mysql /var/lib/mysql
+# else
+# 	chown -R mysql:mysql /var/lib/mysql
+# 	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+# 	mariadbd --user=mysql --bootstrap --skip-networking=0 << eof
+# USE mysql;
+# FLUSH PRIVILEGES;
+# GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+# GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
+# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');
+# DROP DATABASE IF EXISTS test;
+# FLUSH PRIVILEGES;
+# CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+# GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+# FLUSH PRIVILEGES;
+# eof
+
+# fi
+
+# exec mariadbd --user=mysql --skip-networking=0
